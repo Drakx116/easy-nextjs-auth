110bb803e62db017d8101162385e2402e66e4a5e
Drakx116 <alexandre2.guyot@gmail.com>
2020-03-23 01:59:54 +0100

    Add body-parser and cors

--- server/server.js
+++ server/server.js
@@ -1,8 +1,14 @@
 const ENV = require('dotenv').config().parsed;
 const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 const port = 5000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cors());
 
 app.listen(port,
     () => console.log(`Server is running on http://${ ENV.HOST }:${ ENV.PORT }`)
