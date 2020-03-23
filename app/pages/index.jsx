37f8f428d766846b2710df14b6f7efecfc32ef54
Drakx116 <alexandre2.guyot@gmail.com>
2020-03-23 01:40:19 +0100

    Add Header

--- app/pages/index.jsx
+++ app/pages/index.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Header from "../components/Header";
 
 const Index = () => {
     return (
         <div className="container">
             <div className="page">
+                <Header />
                 <h1 className={"page-title"}>
                     Home
                 </h1>
