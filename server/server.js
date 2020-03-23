fb6857bd3ad2ce08317fef960f1738628f6615e8
Drakx116 <alexandre2.guyot@gmail.com>
2020-03-23 16:08:46 +0100

    Spacing

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const ENV = require('dotenv').config().parsed;
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+
 const handlers = require('./middlewares/handlers');
 const routes = require('./routes');
 const db = require('./models');
@@ -11,8 +12,8 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
-app.use(db);
 
+app.use(db);
 app.use('/', routes.alive );
 app.use('/auth', routes.auth );
 
