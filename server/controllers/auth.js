4f6268bde61356041f30b9eaab77a3cd24c3e0f2
Drakx116 <alexandre2.guyot@gmail.com>
2020-03-23 17:45:37 +0100

    Update pseudo declaration + Spacing

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -4,26 +4,27 @@ const ENV = require('dotenv').config().parsed;
 const { User } = require('../models/User');
 
 export const login = (req, res) => {
-    // Change to req.body.pseudo
-    const pseudo = 'test-pseudo';
+    const pseudo = req.body.pseudo;
 
     if(!pseudo) {
-        return res.status(400).json({ 'message': 'Missing params.' });
+        return res.status(406).json({ 'error': 'Missing params.' });
     }
 
     User.findOne({ 'pseudo': pseudo }, (error, user) => {
         if(!user) {
-            return res.status(404).json({ 'message': 'Cannot find any related user.' });
+            return res.status(404).json({ 'error': 'Cannot find any related user.' });
         }
 
-        const token = jwt.sign({ 'pseudo': user.pseudo, id: user.id }, `${ ENV.JWT_SECRET }`, { expiresIn: 1200 } );
+        const token = jwt.sign({ 'pseudo': user.pseudo, id: user.id }, `${ ENV.JWT_SECRET }`, { expiresIn: 1200 });
 
         res.status(200).json({
-            'user': {
-                'id': user.id,
-                'pseudo': user.pseudo
-            },
-            'token': token,
+            'data': {
+                'user': {
+                    'id': user.id,
+                    'pseudo': user.pseudo
+                },
+                'token': token,
+            }
         });
     });
 };
