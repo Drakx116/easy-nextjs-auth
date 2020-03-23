2f699305fa840fc552564a0adc4c2a2ffe0cd3b0
Drakx116 <alexandre2.guyot@gmail.com>
2020-03-23 01:33:52 +0100

    Init NextJS app

---
+++ app/pages/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Index = () => {
+    return (
+        <div className="container">
+            <div className="page">
+                <h1 className={"page-title"}>
+                    Home
+                </h1>
+            </div>
+        </div>
+    );
+};
+
+export default Index;
